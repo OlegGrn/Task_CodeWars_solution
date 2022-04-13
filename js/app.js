adce2e91ad81ef39a32dad1106a313d76dade821
OlegGrn <grudinol@mail.ru>
2022-04-13 13:28:44 +0200

    13 04 13-28

--- js/app.js
+++ js/app.js
@@ -1,62 +1,6 @@
 "use strict";
 
-// let nameUser = prompt('Whot your name?', '');
-// console.log(`User name: ${nameUser}`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 2; i < 10; i++) {
-// 	if (!(i % 2 === 0)) continue;
-// 	alert(i)
-// }
-
-
-// let value = NaN;
-// do {
-// 	value = prompt("number?")
-// 	console.log(+value);
-
-// } while ((value < 100 && value !== null) || isNaN(value))
-
-// function min(a, b) {
-// 	return ((a - b) >= 0) ? alert(b) : alert(a);
-// }
-
-// function pow(x, n) {
-// 	let risult = x;
-// 	if (n == 1) return risult;
-// 	for (let i = 2; i <= n; i++) {
-// 		risult *= x;
-// 	}
-// 	return risult;
-// }
 
 
 
