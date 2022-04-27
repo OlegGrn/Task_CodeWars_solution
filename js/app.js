0e3927bbd3ae13d52cc9b3ad196e5b87d5be29be
OlegGrn <grudinol@mail.ru>
2022-04-27 19:41:25 +0200

    19-41

--- js/app.js
+++ js/app.js
@@ -1,77 +1,38 @@
 "use strict";
 
-//*
-/*Создайте функцию, принимающую положительное целое число в качестве параметра и
-возвращающую строку, содержащую римское числовое представление этого целого числа.*/
 
-// function solution(number) {
 
-// 	let strnumber = String(number).split('');
+function validParentheses(parens) {
 
-// 	let numberRim = [
-// 		['I', 'V', 'X'],
-// 		['X', 'L', 'C'],
-// 		['C', 'D', 'M'],
-// 		['M']
-// 	];
-
-// 	let rimnumberer = [];
-
-// 	strnumber.forEach((item, index, arstr) => {
-// 		addResult(item, numberRim[arstr.length - ++index], rimnumberer);
-// 	});
-
-// 	function addResult(number, arr, result) {
-// 		let first = 1;
-// 		if (number == 0) {
-// 			return
-// 		} else if (number > 5 && number < 9) {
-// 			result.push(arr[1]);
-// 			while ((number - 4) - first++) result.push(arr[0]);
-// 		} else if (number >= 1 && number < 4) {
-// 			while ((+number + 1) - first++) result.push(arr[0]);
-// 		} else if (number == 4) {
-// 			result.push(arr[0]);
-// 			result.push(arr[1]);
-// 		} else if (number == 9) {
-// 			result.push(arr[0]);
-// 			result.push(arr[2]);
-// 		} else result.push(arr[1]);
-
-// 	}
-// 	return rimnumberer.join('');
-// }
-
-function solution(number) {
-	// convert the number to a roman numeral
-	let roman = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
-	let ans = '';
-
-	for (let key in roman) {
-		while (number >= roman[key]) {
-			ans = ans + key;
-			number = number - roman[key];
-		}
-	}
-	return ans;
+	return parens.split('').every((el, index, arr) => {
+		return (el !== arr[arr.length - 1 - index]);
+	})
 }
 
-let x = solution(111);
+
+
+
+
+
+
+let x = validParentheses("(()()()())(())") // false
+// let e = validParentheses("()") // true
+
 console.log(x);
-
-
-//console.log(String('10').split(''));
-
+// console.log(e);
 
 
 
-
+//return '\u{0028}' + '\u{0029}';
 
 
 
 
 
-// let right = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+
+
+
+
 
 
 
--- js/done.js
+++ js/done.js
@@ -1,3 +1,101 @@
+//*
+/*Напишите функцию, которая найдет все анаграммы слова из списка. Вам будет дано два входа слово и 
+массив со словами. Вы должны вернуть массив всех анаграмм или пустой массив, если их нет.*/
+//? Вариант 2
+
+function anagrams(word, words) {
+	let result = [];
+	words.forEach(item => {
+		if (word.length != item.length) return;
+		if (word.split('').sort().join('') == item.split('').sort().join('')) result.push(item);
+	});
+	return result;
+}
+
+
+//? Вариант 1
+function anagrams(word, words) {
+
+	let result = [];
+	let length = word.length;
+
+	outer:
+	for (let i = 0; i < words.length; i++) {
+
+		if (word.length != words[i].length) continue;
+
+		let maybe = Array.from(words[i]);
+		let mask = Array.from(word);
+
+		for (let n = 0; n < length; n++) {
+
+			let index = maybe.indexOf(mask[0]);
+			if (index === -1) continue outer;
+			mask.splice(0, 1);
+			maybe.splice(index, 1);
+
+		}
+		result.push(words[i])
+	}
+	return result;
+}
+
+//*
+/*Создайте функцию, принимающую положительное целое число в качестве параметра и
+возвращающую строку, содержащую римское числовое представление этого целого числа.*/
+
+function solution(number) {
+
+	let strnumber = String(number).split('');
+
+	let numberRim = [
+		['I', 'V', 'X'],
+		['X', 'L', 'C'],
+		['C', 'D', 'M'],
+		['M']
+	];
+
+	let rimnumberer = [];
+
+	strnumber.forEach((item, index, arstr) => {
+		addResult(item, numberRim[arstr.length - ++index], rimnumberer);
+	});
+
+	function addResult(number, arr, result) {
+		let first = 1;
+		if (number == 0) {
+			return
+		} else if (number > 5 && number < 9) {
+			result.push(arr[1]);
+			while ((number - 4) - first++) result.push(arr[0]);
+		} else if (number >= 1 && number < 4) {
+			while ((+number + 1) - first++) result.push(arr[0]);
+		} else if (number == 4) {
+			result.push(arr[0]);
+			result.push(arr[1]);
+		} else if (number == 9) {
+			result.push(arr[0]);
+			result.push(arr[2]);
+		} else result.push(arr[1]);
+
+	}
+	return rimnumberer.join('');
+}
+// не моё решение
+function solution(number) {
+	// convert the number to a roman numeral
+	let roman = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
+	let ans = '';
+
+	for (let key in roman) {
+		while (number >= roman[key]) {
+			ans = ans + key;
+			number = number - roman[key];
+		}
+	}
+	return ans;
+}
+
 
 
 
