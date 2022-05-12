7e5b6e23e20bcc4aa2cd135b22c7456ec0faaa90
OlegGrn <grudinol@mail.ru>
2022-05-13 00:42:29 +0200

    454456

--- js/app.js
+++ js/app.js
@@ -1,87 +1,137 @@
 "use strict";
 
-function recoverSecret(triplets) {
-
-	function filt(ltr) {
-		return triplets1
-			.filter((item) => item.includes(ltr))
-			.reduce((max, el) => {
-				return (el.indexOf(ltr) >= max.indexOf(ltr)) ? el : max;
-			}, [`${ltr}`, 0, 0])
-			.filter((it, ind, arr) => ind == arr.indexOf(ltr) - 1);
-	}
-
-	//let newLtr = filt('h');
-	// while (newLtr.length) console.log('ff');
-
-	// console.log(newLtr);
-	// console.log(...newLtr);
-
-	let value = [];
-
-	triplets.forEach(item => {
-		item.forEach((e, u, y) => {
-
-			let result = [e];
-			let newLtr = filt(e);
-
-			while (newLtr.length) {
-				result.unshift(...newLtr);
-				newLtr = filt(...newLtr)
+function selectLeter(leter, arr) { // получаем буквы идущие сразу же за leter 
+	return arr
+		.filter(row => row.includes(leter))
+		.map(item => {         // получаем массив букв сразу же за Leter => _leter
+			if (item.indexOf(leter) != 3) {
+				return item[item.indexOf(leter) + 1]
 			}
-
-			value.push(...result);
-
 		})
+		.filter((_l, _i, _a) => _i == _a.indexOf(_l) && _l !== undefined) // убираем повторы		
+}
 
-	});
+function start(arr) { // получаем первую букву
+	return arr
+		.map(item => item[0])
+		.filter(first => {
+			let pos = arr.reduce((index, row) => {
+				let _ind = 0
+				if (row.includes(first)) {
+					_ind = row.indexOf(first);
+				}
+				return index += _ind;
+			}, 0)
+			return pos == 0;
+		})[0];
+}
 
-	console.log(value);
+function recoverSecret(triplets) {
+	let word = [];
+	let firstLeter = start(triplets);
+	word = word.concat(firstLeter);
+
+
+	let nextLeters = selectLeter(firstLeter, triplets);
+	console.log(nextLeters);
+
+
+	return word;
 }
 
 
 
 
+
+
+
+
 let triplets1 = [
-	['t', 'u', 'p'],
 	['w', 'h', 'i'],
 	['t', 's', 'u'],
 	['a', 't', 's'],
+	['w', 'h', 's'],
 	['h', 'a', 'p'],
 	['t', 'i', 's'],
-	['w', 'h', 's']
+	['t', 'u', 'p']
 ]
-//console.log(recoverSecret(triplets1));
 
-recoverSecret(triplets1);
-
-// let index = log
-// 			.map(item => item.indexOf(a))
-// 			.sort()
-// 			.reverse()
-// 			.slice(0, 1);
-
-// .filter((e, i, ar) => {
-		// 	if (ar.length > 1 && ar[i + 1]) {
-		// 		console.log(Math.max(e.indexOf(ltr), ar[i + 1].indexOf(ltr)));
-		// if (e.indexOf(ltr) > ar[i - 1].indexOf(ltr)) {
-		// 	return true
-		// }
-		// if (ar[i - 1] !== undefined) {
-		// 	return e.indexOf(ltr) > ar[i - 1].indexOf(ltr)
-		// }
-
-		// 	} else return true
-		// })
-		//.map(it => it[it.indexOf(ltr) - 1])
-
-		//   ['w', 'h', 'i'],
-		//   ['t', 'i', 's']
+console.log(recoverSecret(triplets1));
 
 
 
-		// .find((array) => {
-		// 	if (array.length > 0) {
 
-		// 	}
-		// })
+// function recoverSecret(triplets) {
+
+// 	function filt(ltr, res) {
+// 		return triplets1
+// 			.filter((item) => item.includes(ltr))
+// 			.reduce((max, el, index, array) => {
+
+// 				if (array.length == 1) {
+// 					return el;
+// 				} else if (el.indexOf(ltr) >= max.indexOf(ltr)
+// 					&& !res.includes(el[el.indexOf(ltr) - 1])) {
+// 					return el
+// 				} else return max;
+// 			}, [`${ltr}`, 0, 0])
+// 			.filter((it, ind, arr) => ind == arr.indexOf(ltr) - 1);
+// 	}
+
+// 	let value = [];
+
+// 	triplets.forEach(item => {
+// 		item.forEach((e, u, y) => {
+
+// 			let result = [e];
+// 			let newLtr = filt(e, value);
+
+// 			while (newLtr.length) {
+// 				result.unshift(...newLtr);
+// 				newLtr = filt(...newLtr, value)
+// 			}
+// 			value.push(...result);
+// 		})
+// 	});
+// 	return value
+// 		.filter((item, index, arr) => arr.indexOf(item) == index)
+// 		.join('')
+// }
+
+
+
+// const recoverSecret = (triplets) => {
+// 	let duplets = [];
+// 	let res = '';
+
+// 	/*
+// 	  convert triplets to duplets
+// 	 */
+
+
+// 	triplets.forEach(trip => {
+// 		let str = trip.join('');
+// 		let d1 = str.substr(0, 2);
+// 		let d2 = str.substr(1, 3);
+// 		if (duplets.indexOf(d1) === -1) { duplets.push(d1); }
+// 		if (duplets.indexOf(d2) === -1) { duplets.push(d2); }
+// 	});
+
+// 	let nxt = findNext(duplets);
+// 	while (nxt) {
+// 		res += nxt;
+// 		duplets = duplets.filter(d => d.indexOf(nxt) === -1);
+// 		nxt = findNext(duplets);
+// 	}
+
+// 	return res;
+// }
+
+// /**
+//  * Return the first letter of the secret word.
+//  */
+// function findNext(duplets) {
+// 	// 1st char is not the 2nd char in any duplet
+// 	let nxt = duplets.find(d => duplets.every(_d => d[0] !== _d[1]));
+// 	return duplets.length > 1 ? nxt[0] : duplets[0];
+// }
