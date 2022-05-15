ae62fa66c3ac761f3fb834d6353c780643f21166
OlegGrn <grudinol@mail.ru>
2022-05-15 23:09:56 +0200

    45645645

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 "use strict";
 
+Array.prototype.sameStructureAs = function (other) {
+
+	return this.length == other.length
 
 
+};
+
+
+// should return true
+// [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );          
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ); 
+
+console.log([1, [1, 1]].sameStructureAs([2, [2, 2]]));
+
+
+// should return false 
+//  [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] );  
+//  [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] ); 
+
+
+console.log([1, [1, 1]].sameStructureAs([[2], 2]));
