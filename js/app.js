7e03a5f1e24917188362428f87420f1a634ebcab
OlegGrn <grudinol@mail.ru>
2022-05-18 00:06:06 +0200

    44545

--- js/app.js
+++ js/app.js
@@ -2,9 +2,30 @@
 
 Array.prototype.sameStructureAs = function (other) {
 
-	return this.length == other.length
+	if (
+		Array.isArray(this) &&
+		Array.isArray(other) &&
+		this.every(item => Array.isArray(item) == false) &&
+		other.every(item => Array.isArray(item) == false)
+	) return this.length == other.length
 
+	outer:
+	for (let i = 0; i < Math.max(other.length, this.length); i++) {
+		if (
+			!Array.isArray(this[i]) &&
+			!Array.isArray(other[i])
+		) { continue }
+		else if (
+			(Array.isArray(this[i]) && !Array.isArray(other[i])) ||
+			(!Array.isArray(this[i]) && Array.isArray(other[i])) ||
+			other[i] === undefined ||
+			this[i] === undefined
 
+		) { break outer }
+
+		else return this[i].sameStructureAs(other[i]);
+	}
+	return false;
 };
 
 
@@ -12,7 +33,7 @@ Array.prototype.sameStructureAs = function (other) {
 // [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );          
 // [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] ); 
 
-console.log([1, [1, 1]].sameStructureAs([2, [2, 2]]));
+console.log([2, [1, 1], [1, 2]].sameStructureAs([3, [2, 1], [2, 2], 2]));
 
 
 // should return false 
