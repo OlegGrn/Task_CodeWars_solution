4e0e232b06027f2ccb23fa6a7c4e956d87decf74
OlegGrn <grudinol@mail.ru>
2022-05-30 21:18:35 +0200

    5645

--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
 "use strict";
 
 var countDeafRats = function (town) {
-	// Your code here
-	return 0;
+
+	let result = 0;
+	let start = town.indexOf('P');
+	let arrTown = town.split("");
+
+	let right = arrTown.slice(start + 1).reduce((sum, item) => {
+		(item == "O") ? sum++ : sum--;
+		if (sum < 0) result++;
+		return sum
+	}, 0)
+
+	let left = arrTown.slice(0, start).reduceRight((sum, item) => {
+		(item == "O") ? sum++ : sum--;
+		if (sum < 0) result++;
+		return sum
+	}, 0)
+
+	return result
+
 }
 
 
 
 
 
-console.log(countDeafRats("~O~O~O~OP~O~OO~"));
+console.log(countDeafRats("O~~OPO~O~~O~O"));
 
 
 
