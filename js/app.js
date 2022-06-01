5b411d937d235c46c662c4dec613f0a48065aa0d
OlegGrn <grudinol@mail.ru>
2022-06-01 23:23:17 +0200

    ghghg

--- js/app.js
+++ js/app.js
@@ -2,34 +2,38 @@
 
 function towerBuilder(nFloors) {
 
-	let str = '*';
+	let result = [];
+	let max = nFloors * 2 - 1;
 
+	let n = 1;
+	do {
+		let center = 2 * n - 1;
+		let aside = (max - center) / 2;
+		result.push(' '.repeat(aside) + "*".repeat(center) + ' '.repeat(aside));
+		n++
+	} while (n <= nFloors)
 
-
-	if (nFloors == 1) {
-		return [str.repeat(nFloors)]
-
-	} else {
-		return [towerBuilder(nFloors - 1) + "**"]
-	}
-
-	// let n = (nFloors == 1) ? nFloors : towerBuilder(nFloors - 1) + 2;
-
+	return result
 
 
 
 }
 
-
-
-
-
-console.log(towerBuilder(3));
-
+console.log(towerBuilder(4));
 
 
 
 
 
+/*
+let result = [];
+	let n = 1;
 
+	do {
+		result.push("*".repeat(2 * n - 1))
+		n++
+	} while (n <= nFloors)
+
+	return result
+	*/
 
