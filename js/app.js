a8664373e097abee52a38a2a27f11f3eac5e239b
OlegGrn <grudinol@mail.ru>
2022-06-16 19:00:28 +0200

    аапап

--- js/app.js
+++ js/app.js
@@ -1,23 +1,14 @@
 "use strict";
 
-function csvColumns(csv, indices) {
-	return csv
-		.split('\n')
-		.map(item => item
-			.split(",")
-			.filter((_, index) => indices.includes(index)))
-		.join("\n")
-		.trim()
+function high(x) {
+
 }
 
 
 
 
 
-let csv = "0a,1b,2c,3d,4e\n01,12,23,34,45\n0f,1g,2h,3i,4j";
 
 
-let indices = [5, 7];
-
-console.log(csvColumns(csv, indices));
+console.log(high('man i need a taxi up to ubud'));
 
