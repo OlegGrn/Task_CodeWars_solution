10f3350f875ccc0a481cd2c8ed42f95e6d0272f2
OlegGrn <grudinol@mail.ru>
2022-06-28 21:32:44 +0200

    fvfvff

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 "use strict";
 
 
-function getLengthOfMissingArray(arrayOfArrays) {
+function isPrime(num) {
 
-	if (arrayOfArrays === null
-		|| arrayOfArrays.length == 0
-		|| arrayOfArrays.includes(null)
-		|| arrayOfArrays.find(item => item.length === 0)
-	) return 0
-
-	return arrayOfArrays
-		.map(item => item.length)
-		.sort((a, b) => a - b)
-		.find((item, ind, arr) => (arr[ind + 1] - item) > 1) + 1
+	if (num <= 1) {
+		return false
+	} else if (num == 2 || num == 3) {
+		return true
+	} else {
+		if (num % 2 == 0) return false;
+		for (let i = 3; i < num; i++) {
+			if (num % i == 0) return false;
+			i++
+		}
+		return true
+	}
 }
 
-console.log(getLengthOfMissingArray(null));
+console.log(isPrime(7));
+
+
 
 
