89f5498ad8a4ec38ebe5edc0ba7e85a763c24de1
OlegGrn <grudinol@mail.ru>
2022-07-10 23:28:11 +0200

    23:27 10.07.22

--- js/app.js
+++ js/app.js
@@ -1,24 +1,8 @@
 "use strict";
 
-function luckCheck(ticket) {
-	let arrTicket = ticket.split("");
-	let midl = Math.floor(arrTicket.length / 2);
-	let result = arrTicket.reduce((sum, item, ind) => {
-		if (ind < midl) {
-			sum.left += +item;
-		} else if (ind < (arrTicket.length - ind)) {
-			return sum
-		} else {
-			sum.right += +item;
-		}
-		return sum
-	}, { "left": 0, "right": 0 });
-	return result.left == result.right;
-}
 
 
 
-console.log(luckCheck('6363'));
 
 
 
