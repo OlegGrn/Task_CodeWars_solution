27ad0448eaf2589a3825826938b099fe9a8f15c4
OlegGrn <grudinol@mail.ru>
2022-08-09 18:52:06 +0200

    09 08 2022

--- js/app.js
+++ js/app.js
@@ -1,19 +1,12 @@
 "use strict";
 
 
-var lastDigit = function (str1, str2) {
-	if (str2 == 0) return 1;
-	let lastNumBase = +str1.slice(-1);
-	if (lastNumBase === 1 || lastNumBase === 5 || lastNumBase === 0) return lastNumBase;
-	let restPow = Number(BigInt(str2) % BigInt(4));
-	if (restPow !== 0) return +`${Math.pow(lastNumBase, restPow)}`.slice(-1);
-	let evenOrOdd = (lastNumBase % 2 === 0) ? true : false;
-	if (evenOrOdd) return 6;
-	return 1;
+function incrementString(strng) {
+
 }
 
 
-console.log(lastDigit("9", "7"));
+console.log(incrementString("foo099"));
 
 
 
