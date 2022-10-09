b50bde740db55cf9a27daebcbe4041bff628d6ea
OlegGrn <grudinol@mail.ru>
2022-10-09 23:52:52 +0200

    09 10 2022

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 "use strict";
 
-l
+let form = document.querySelector('form');
+
+function maskPhone(form) {
+	const input = form.querySelector('input');
+	let val = input.value
+	console.log(val);
+
+	let mask = '__ __';
+	let i = 0;
+	let new_value = mask.replace(/[_]/g, (a) => {
+		return i < val.length ? val.charAt(i++) : 1;
+	})
+
+
+	console.log(new_value)
+
+
+
+
+
+
+
+}
+
+
+
+
+form.addEventListener('input', () => maskPhone(form));
+
 
 
 
