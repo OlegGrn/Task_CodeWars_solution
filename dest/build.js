f1a013121a1c3bb1f04aa852aa3bfc419d44c1ad
OlegGrn <grudinol@mail.ru>
2023-03-12 20:22:24 +0200

    12 03 2023

--- dest/build.js
+++ dest/build.js
@@ -1,17 +1,2 @@
 "use strict";
-function toCamelCase(str) {
-    let count;
-    return str.replace(/./g, (char, ind) => {
-        if (char === "-" || char === "_") {
-            count = ind + 1;
-            return "";
-        }
-        else if (ind === count) {
-            return char.toUpperCase();
-        }
-        else {
-            return char;
-        }
-    });
-}
-console.log(toCamelCase("the_stealth-warrior"));
+console.log();
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
-function toCamelCase(str: string): string {
-    let count: number;
-    return str.replace(/./g,
-        (char: string, ind: number) => {
-            if (char === "-" || char === "_") {
-                count = ind + 1;
-                return ""
-            } else if (ind === count) {
-                return char.toUpperCase()
-            } else {
-                return char
-            }
-        })
-}
+
 
 
 console.log(
-    toCamelCase("the_stealth-warrior")
+
 )
 
 
