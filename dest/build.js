f1f95fde1e7f23390429d8908b2a5712fe9206e9
OlegGrn <grudinol@mail.ru>
2023-03-31 18:14:31 +0200

    31 03 2023 regExp

--- dest/build.js
+++ dest/build.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log();
+function decipherThis(str) {
+    return str
+        .split(" ")
+        .map(word => {
+        return word
+            .replace(/\d+/, digit => String.fromCodePoint(+digit))
+            .split("")
+            .map((char, ind, arr) => {
+            if (ind === 1) {
+                return arr[arr.length - 1];
+            }
+            else if (ind === arr.length - 1 && arr.length > 1) {
+                return arr[1];
+            }
+            else {
+                return char;
+            }
+        })
+            .join("");
+    })
+        .join(" ");
+}
+console.log(decipherThis("Hello word"));
