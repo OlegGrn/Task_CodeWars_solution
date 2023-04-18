f07db15981def68cb90759c172a3eff7352f38c9
OlegGrn <grudinol@mail.ru>
2023-04-18 19:26:07 +0200

    19-25 18 04 2023 regExp

--- dest/build.js
+++ dest/build.js
@@ -1,7 +1,1 @@
 "use strict";
-function doTest(str) {
-    const reg = /^[A-Za-z\d](?=.*[a-z])[A-Za-z\d]{5,}$/;
-    console.log(str.match(reg));
-    return reg.test(str);
-}
-console.log(doTest("8l5S3KHS27"));
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
 
-function doTest(str: string){
-    const reg =/^[A-Za-z\d](?=.*[a-z])[A-Za-z\d]{5,}$/ ;
-
-    console.log(str.match(reg))
-
-    return reg.test(str)
-}
-
-console.log(doTest("8l5S3KHS27"))
