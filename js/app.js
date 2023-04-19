bf62a7d4b03f5c217eb0198bd33af9bf27a53512
OlegGrn <grudinol@mail.ru>
2023-04-19 18:34:45 +0200

    18-33 19 04 2023 regExp

--- js/app.js
+++ js/app.js
@@ -1,27 +1,5 @@
 "use strict";
 
-function initials(n) {
-	let reg = /\b(\w(?=.*\s)|(\w)(\w+)(?=\w*$))/g
-
-
-	console.log(n.match(reg))
-	return n.replace(reg, (_, p1, p2, p3) => {
-		console.log(_);
-		console.log(p1);
-		console.log(p2);
-
-		console.log(p3);
-
-
-		let test = !p2 ? p1.toUpperCase() : p2.toUpperCase() + p3;
-
-		return test
-
-	})
-
-}
-
-console.log(initials("barack hussain obama"));
 
 
 
