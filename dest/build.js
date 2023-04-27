e25f55230c1f1db12e2129fde3d10b805c311eca
OlegGrn <grudinol@mail.ru>
2023-04-27 18:34:41 +0200

    18-34 27 04 2023 regExp

--- dest/build.js
+++ dest/build.js
@@ -1,12 +1,1 @@
 "use strict";
-function loopArr(arr, direction, steps) {
-    if (direction === "left") {
-        let arr2 = arr.splice(0, steps);
-        return [...arr, ...arr2];
-    }
-    else if (direction === "right") {
-        let arr2 = arr.splice(-steps);
-        return [...arr2, ...arr];
-    }
-}
-console.log(loopArr([1, 5, 87, 45, 8, 8], 'right', 2));
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-function loopArr(arr: number[], direction: string, steps: number) {
 
-    if (direction === "left") {
-        let arr2: number[] = arr.splice(0, steps);
-        return [...arr, ...arr2]
-
-    } else if (direction === "right") {
-        let arr2: number[] = arr.splice(-steps)
-        return [...arr2, ...arr]
-    }
-}
-
-console.log(loopArr([1, 5, 87, 45, 8, 8], 'right', 2))
