e62245a5264fae9de8542e8c89143dbcc00abdb1
OlegGrn <grudinol@mail.ru>
2023-06-06 15:14:52 +0200

    26 06 shtorm 2023 regExp

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function saveLinkActive() {
+/*function saveLinkActive() {
 	// получаем путь из адресной строки
 	const path = document.location.pathname.replace("/", "")
 	// находим соответствующий путь в меню в сылках и добавляем класс найденной ссылке
@@ -11,6 +11,20 @@ function saveLinkActive() {
 		})
 	}
 
+}*/
+
+function saveLinkActive() {
+	// получаем путь из адресной строки
+	const path = document.location.pathname
+	// находим соответствующий путь в меню в сылках и добавляем класс найденной ссылке
+	const links = document.querySelectorAll(".menu__link");
+	if (links) {
+		links.forEach(link => {
+			if (link.pathname.localeCompare(path) === 0) {
+				link.classList.add("menu__link_active")
+			}
+		})
+	}
 }
 
 
