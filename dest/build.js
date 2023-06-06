867f4fed2ed0f9063653fa4b43b8f38578447254
OlegGrn <grudinol@mail.ru>
2023-06-06 14:15:53 +0200

    06 06 2023 test

--- dest/build.js
+++ dest/build.js
@@ -1,8 +1,1 @@
-"use strict";
-var solution = function (firstArray, secondArray) {
-    let res = firstArray.reduce((sum, it, ind) => {
-        return sum + (it - secondArray[ind]) ** 2;
-    }, 0) / firstArray.length;
-    console.log(res);
-};
-solution([1, 2, 3], [4, 5, 6]);
+
---
+++ js/main.js
@@ -0,0 +1,17 @@
+function saveLinkActive() {
+	// получаем путь из адресной строки
+	const path = document.location.pathname.replace("/", "")
+	// находим соответствующий путь в меню в сылках и добавляем класс найденной ссылке
+	const links = document.querySelectorAll(".menu__link");
+	if (links) {
+		links.forEach(link => {
+			if (link.getAttribute("href").localeCompare(path) === 0) {
+				link.classList.add("menu__link_active")
+			};
+		})
+	}
+
+}
+
+
+document.addEventListener("DOMContentLoaded", saveLinkActive);
