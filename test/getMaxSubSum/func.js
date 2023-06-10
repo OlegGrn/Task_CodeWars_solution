585799a2aa056dfee3c59e4738b6eaae885ff2e3
OlegGrn <grudinol@mail.ru>
2023-06-10 15:31:33 +0200

    10 06 2023 15-22

--- test/getMaxSubSum/func.js
+++ test/getMaxSubSum/func.js
@@ -2,15 +2,22 @@
 // Задача: найти непрерывный подмассив в arr, сумма элементов в котором максимальна.
 // Функция getMaxSubSum(arr) должна возвращать эту сумму.
 
-
 function getMaxSubSum(arr) {
-
-	let _1 = [-1, 2, 3, -9];
-
-
-
-
+    let test = [];
+    while (arr.length > 0) {
+        arr.reduceRight((sum, it) => {
+            sum = sum + it;
+            test.push(sum);
+            return sum;
+        }, 0);
+        arr.pop();
+    }
+    let result = Math.max(...test);
+    return result > 0 ? result : 0;
 
 }
+
+
+
 
 
