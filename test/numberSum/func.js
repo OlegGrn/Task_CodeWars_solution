7128632affcb84a0fdc6aee9e85b500b1153c929
OlegGrn <grudinol@mail.ru>
2023-07-03 22:25:27 +0200

    03 07 2023 22-25

--- test/numberSum/func.js
+++ test/numberSum/func.js
@@ -1,3 +1,8 @@
-function numberSum() {
-    return 1
+function numberSum(arr) {
+	return arr.reduce((sum, a, i, ar) => {
+		for (let it = ++i; it < ar.length; it++) {
+			sum += + (a + ar[it])
+		}
+		return sum
+	}, 0)
 }
