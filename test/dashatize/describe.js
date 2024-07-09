b5980afd07901cfa65ae95b3fc126306305b8db4
OlegGrn <grudinol@mail.ru>
2024-07-09 17:58:49 +0200

    09 07 2024 17-58

--- test/dashatize/describe.js
+++ test/dashatize/describe.js
@@ -1,23 +1,24 @@
-describe("dashatize", function (){
+describe("dashatize", function () {
 
     describe(`Для заданного целого числа вернуть строку с дефисами '-'до и после каждой нечетной цифры,
-     но не начинать и не заканчивать строку дефисом.`, function (){
+     но не начинать и не заканчивать строку дефисом.`, function () {
 
         let arrTest = [
             [274, "2-7-4"],
             [5311, "5-3-1-1"],
             [86320, "86-3-20"],
-            [974302, "9-7-4-3-02"]
+            [974302, "9-7-4-3-02"],
+            [-1, "1"]
         ]
 
-        function makeTest(oneTest){
+        function makeTest(oneTest) {
             const [params, result] = oneTest;
-            it(`принимаем ${params}, результат ${result}`, function (){
+            it(`принимаем ${params}, результат ${result}`, function () {
                 assert.strictEqual(dashatize(params), result)
             })
         }
 
-        arrTest.forEach( item => makeTest(item))
+        arrTest.forEach(item => makeTest(item))
     })
 
 })
--- test/dashatize/func.js
+++ test/dashatize/func.js
@@ -1,5 +1,5 @@
-
-
-function dashatize(){
-
+function dashatize(num) {
+    return num.toString()
+        .replace(/./g, match => match % 2 ? "-" + match + "-" : match)
+        .replace(/(^--|^-)|(--)|(-$)/g, (_, _1, _2) => _2 ? "-" : "")
 }
