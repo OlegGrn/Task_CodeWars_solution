c897f708a0daf399a815b93b0c3e13529983ba49
OlegGrn <grudinoleg@gmail.com>
2024-09-30 22:07:28 +0200

    30 09 2024 22-07

--- dest/typeScript.js
+++ dest/typeScript.js
@@ -1,5 +1,21 @@
 "use strict";
-function cuckoo(str, num) {
-    let arr = str.match(/\d{2}/g) || [];
+function cuckoo(inputTime, chimes) {
+    let arr = inputTime.match(/\d{2}/g) || [];
     let [hours, min] = arr.map(i => +i);
+    min = min % 15 ? Math.ceil(min / 15) * 15 : min;
+    while (chimes > 0) {
+        if (min === 0 || min === 60) {
+            hours = min === 60 ? (hours === 12 ? 1 : ++hours) : hours;
+            chimes -= hours;
+            min = chimes > 0 ? 15 : 0;
+        }
+        else {
+            chimes--;
+            min = chimes > 0 ? min + 15 : min;
+        }
+    }
+    let newHours = hours.toString().padStart(2, "0");
+    let newMin = min.toString().padStart(2, "0");
+    return (newHours + ":" + newMin);
 }
+console.log(cuckoo("03:38", 19));
--- src/typeScript.ts
+++ src/typeScript.ts
@@ -1,12 +1,32 @@
 
-function cuckoo(str: string, num: number){
+function cuckoo(inputTime: string, chimes: number){
 
-    let arr: string[] = str.match(/\d{2}/g) || [];
-    let [hours, min] = arr.map<>(i => +i);
+    let arr: string[] = inputTime.match(/\d{2}/g) || [];
+    let [hours, min] = arr.map(i => +i);
 
+    min = min % 15 ? Math.ceil(min/15) * 15: min;
 
+    while (chimes > 0){
+        if(min === 0 || min === 60){
+            hours = min === 60? (hours === 12? 1: ++hours) : hours;
+            chimes -= hours;
+            min = chimes > 0? 15: 0
+        } else {
+            chimes--;
+            min = chimes > 0? min + 15: min;
+        }
+    }
+    let newHours: string = hours.toString().padStart(2, "0")
+    let newMin: string = min.toString().padStart(2, "0")
 
-
-
-
+    return (newHours + ":" + newMin)
 }
+
+console.log(
+    cuckoo("03:38", 19)
+)
+
+
+
+
+
